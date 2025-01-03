235a626fe23819b1d7d74b5e49ad07983c819d63
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2025-01-04 01:17:42 +0700

    Impement signup, login, logout. Protect route middleware, connect DB, and cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,21 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(4001, () => {
-    console.log("Server is running on port 4001");
-    
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
+    connectDB();
 })
