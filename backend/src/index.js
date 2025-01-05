0cef193e33911a7e070e0d5f3fb6cfd6583bae16
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2025-01-05 13:15:19 +0700

    Implement Socket.io in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,10 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT;
 
 app.use(cookieParser());
@@ -21,7 +22,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
     connectDB();
 })
