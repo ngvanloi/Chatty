d0e07795ccfed0a3d5c79ea8c7fb334c3de95598
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2025-01-05 14:03:56 +0700

    Detect message send/receive in realtime for frontend

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -44,19 +44,18 @@ export const useChatStore = create((set, get) => ({
   subscribeToMessages: () => {
     const { selectedUser } = get();
     if (!selectedUser) return;
-    // const socket = useAuthStore.getState().socket;
-    // socket.on("newMessage", (newMessage) => {
-    //   const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
-    //   if (!isMessageSentFromSelectedUser) return;
+    const socket = useAuthStore.getState().socket;
 
-    //   set({
-    //     messages: [...get().messages, newMessage],
-    //   });
-    // });
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSendFromSelectedUser = newMessage.senderId !== selectedUser._id
+      if (!isMessageSendFromSelectedUser) return
+      set({ messages: [...get().messages, newMessage] })
+    })
   },
   unsubscribeFromMessages: () => {
-    // const socket = useAuthStore.getState().socket;
-    // socket.off("newMessage");
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage")
   },
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
