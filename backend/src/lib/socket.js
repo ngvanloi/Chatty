9dd91c17d77d69399221ef11dcda1a6a304e00cf
DESKTOP-MBN6FJJ\Admin <nguyenloi.itse@gmail.com>
2025-01-05 13:25:58 +0700

    Detect online users in realtime for backend

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,14 +12,21 @@ const io = new Server(server, {
     }
 })
 
-io.on("connection", (socket) =>{
+const userSocketMap = {};
+
+io.on("connection", (socket) => {
     console.log("A user connected ", socket.id);
+    const userId = socket.handshake.query.userId;
+    if (userId) userSocketMap[userId] = socket.id
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
 
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
-        
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
-    
+
 })
 
 export { io, app, server }
